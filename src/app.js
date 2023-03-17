e652e7c942b47b97609e1ae847220778f8c802a5
Daniloemejias <daniloemejias@gmail.com>
2023-03-17 23:07:17 +0000

    Domain-name-generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+//write your code here
+var pronoun = ["the", "our", "they", "your", "it", "mine"];
+var adj = ["great", "big", "handsome", "happy", "bitter", "deep", "dry"];
+var noun = ["jogger", "racoon", "sushi", "hole", "frog", "games", "jacket"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(i);
+      console.log(j);
+      console.log(k);
+      document.writeln(pronoun[i] + adj[j] + noun[k] + ".com" + "<br/>");
+    }
+  }
+}
